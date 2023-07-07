5844ab4a155e4df011e37d5b765c717e0dacbd10
Parth4629 <137050522+Parth4629@users.noreply.github.com>
2023-07-07 10:13:18 +0530

    PHP Day-1

---
+++ 2101031000163/Day 13/2.php
@@ -0,0 +1,9 @@
+<?php
+$name = "Parth patel";
+$number = 2101031000163;
+$department = "B.tech IT";
+
+echo "Name: " . $name . "<br>";
+echo "Number: " . $number . "<br>";
+echo "Department: " . $department . "<br>";
+?>
